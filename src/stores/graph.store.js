60b3b71ca4ba9d00f234e54f5212aa52211ce916
Michael Ducharm <ducharmdev@gmail.com>
2021-03-28 22:06:28 -0400

    add title graph to sections array

--- src/stores/graph.store.js
+++ src/stores/graph.store.js
@@ -1,24 +1,8 @@
 import { writable } from "svelte/store";
 
-let initialGraph = [
-    // list of graph elements to start with
-    {
-      // node a
-      data: { id: "a" },
-    },
-    {
-      // node b
-      data: { id: "b" },
-    },
-    {
-      // edge ab
-      data: { id: "ab", source: "a", target: "b" },
-    },
-  ];
-
 const initial = {
-    elements: initialGraph,
-    layout: "circle"
+    elements: [],
+    layout: "cola"
 }
 
 export const graphStore = writable(initial);
