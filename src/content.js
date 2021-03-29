c070b2fb48e36d2a60516ce30e071a3fd52280e1
Michael Ducharm <ducharmdev@gmail.com>
2021-03-28 21:05:47 -0400

    separate content

---
+++ src/content.js
@@ -0,0 +1,98 @@
+
+export let sections = [
+    {
+        title: `What is a graph?`,
+        contents: `
+      A graph is a collection of dots and lines, also known these are vertices
+      (or nodes) and edges.
+      `,
+        graph: [
+            {
+                // node a
+                data: { id: "a" },
+            },
+            {
+                // node b
+                data: { id: "b" },
+            },
+            {
+                // edge ab
+                data: { id: "ab", source: "a", target: "b" },
+            },
+        ],
+        el: null,
+    },
+    {
+        title: `What is a graph?`,
+        contents: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+      `,
+        graph: [
+            {
+                data: { id: "a" },
+            },
+            {
+                data: { id: "b" },
+            },
+            {
+                data: { id: "c" },
+            },
+            {
+                // edge ab
+                data: { id: "ab", source: "a", target: "b" },
+            },
+        ],
+        el: null,
+    },
+
+    {
+        title: `What is a graph?`,
+        contents: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+      `,
+        graph: [],
+        el: null,
+    },
+
+    {
+        title: `What is a graph?`,
+        contents: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+      `,
+        graph: [],
+        el: null,
+    },
+
+    {
+        title: `What is a graph?`,
+        contents: `
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+      `,
+        graph: [],
+        el: null,
+    },
+];
